809de43e5cf644773a8595b2d1d9f03f24363940
Court Ewing <court@epixa.com>
2010-05-13 17:47:35 -0400

    Updated index file

--- public/index.php
+++ public/index.php
@@ -1,2 +1,44 @@
 <?php
 
+if (!defined('APPLICATION_ROOT')) {
+    define('APPLICATION_ROOT', realpath(implode(DIRECTORY_SEPARATOR, array(
+        dirname(__FILE__),
+        '..'
+    ))));
+}
+
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(implode(DIRECTORY_SEPARATOR, array(
+        APPLICATION_ROOT,
+        'application'
+    ))));
+}
+
+if (!defined('APPLICATION_ENV')) {
+    if (!$env = getenv('APPLICATION_ENV')) {
+        $env = 'production';
+    }
+    
+    define('APPLICATION_ENV', $env);
+}
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(implode(DIRECTORY_SEPARATOR, array(
+        APPLICATION_ROOT,
+        'libraries'
+    ))),
+    realpath(implode(DIRECTORY_SEPARATOR, array(
+        APPLICATION_ROOT,
+        'vendors',
+        'Zend',
+        'library'
+    ))),
+    get_include_path(),
+)));
+
+require 'Epixa/Application.php';
+$application = new Epixa\Application(
+    APPLICATION_ENV
+);
+
+$application->bootstrap()->run();
